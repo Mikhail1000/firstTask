6c86433218642ac750f702a427214c6d4eb112c0
MT1000 <MT.1000@yandex.ru>
2021-10-30 17:56:13 +0500

    Начал 3-е задание, добавил поле ввода

--- app/src/main/java/com/example/firsttask/MainActivity.java
+++ app/src/main/java/com/example/firsttask/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.firsttask;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView entryFieldCalc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.constraint_layout_calculator);
+
+        entryFieldCalc = findViewById(R.id.entryFieldCalc);
+
     }
 }
